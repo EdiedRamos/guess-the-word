5072934fd07b6e4e4709af5f4a4cc5764609493f
EdiedRamos <edieisa22@gmail.com>
2024-05-14 03:20:01 -0500

    chore: rmeove commented code

--- src/main.js
+++ src/main.js
@@ -3,18 +3,6 @@ import { WordsService } from "./services/words.service.js";
 import { interfaceService } from "./services/interface.service.js";
 
 function main() {
-  // console.log(`${scrambled.original} ${scrambled.scrambled}`);
-  // interfaceService.setScrambledWord(scrambled.scrambled);
-  // interfaceService.setTriesCounter(0);
-  // interfaceService.setTriesCircles();
-  // interfaceService.renderMistakes(["a", "b", "c"]);
-  // interfaceService.setInitialInputs(scrambled.original.length);
-  // interfaceService.addListeners();
-  // interfaceService.setCurrentInput(0);
-  // interfaceService.setTriesCirclesUsed(1);
-  // interfaceService.randomEventListener(() => console.log("JAJAJAJ"));
-  // interfaceService.resetEventListener(() => console.log("JOJOJO"));
-
   const wordService = new WordsService();
   const machineService = new MachineService(interfaceService, wordService);
   machineService.resetGame();
